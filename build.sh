307e52facbf6f5624c455ed713f7422872cfb69d
Florian Bruggisser <florian@bildspur.ch>
2018-11-03 20:18:21 +0100

    fixed error in library format

--- build.sh
+++ build.sh
@@ -22,19 +22,18 @@ cd ../..
 mkdir build && cd build
 
 # create libe57 prebuilt folder
-mkdir -p libE57Format/lib
-mkdir -p libE57Format/include/E57Format
-cp ../contrib/libE57Format/libE57Format.a libE57Format/lib/libE57Format.a
-cp -R -a ../contrib/libE57Format/include/. libE57Format/include/E57Format
+mkdir -p E57Format-2.0-x86_64-darwin/lib
+mkdir -p E57Format-2.0-x86_64-darwin/include/E57Format
+cp ../contrib/libE57Format/libE57Format.a E57Format-2.0-x86_64-darwin/lib/libE57Format.a
+cp -R -a ../contrib/libE57Format/include/. E57Format-2.0-x86_64-darwin/include/E57Format
 
 # prepare cmake
 XERCES_ROOT="/usr/local/Cellar/xerces-c/3.2.2" CMAKE_PREFIX_PATH="/usr/local/Cellar/qt/5.11.2/lib/cmake/" \
 ccmake \
 -D OPTION_USE_LIBE57FORMAT="ON" \
 -D QT5_ROOT_PATH="/usr/local/Cellar/qt/5.11.2/" \
--D LIBE57FORMAT_INSTALL_DIR="libE57Format/" \
--D LIBE57FORMAT_LIBRARY_DEBUG="libE57Format/" \
--D LIBE57FORMAT_LIBRARY_RELEASE="libE57Format/" \
+-D LIBE57FORMAT_INSTALL_DIR="E57Format-2.0-x86_64-darwin/" \
+-D LIBE57FORMAT_LIBRARY_DEBUG="E57Format-2.0-x86_64-darwin/lib/libE57Format.a" \
 ..
 
 make
